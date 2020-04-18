6fce797a8fef4c022bb49495a9a93b0190d781e6
Андрей Грач <holymolly@Andrejs-MacBook-Air.local>
2020-04-18 19:53:48 +0300

    Test

--- web/index.php
+++ web/index.php
@@ -7,19 +7,41 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+    return "Hello home";
+});
+
+$app->post('/bot', function () use ($app) {
+    $data = json_decode(file_get_contents('php://input'));
+    if (!$data) {
+        return "Missed data";
+    }
+    if (!$data->secret !== getenv('VK_SECRET_KEY') && $data->type !== 'confirmation')
+        return "Getenv or dataType error";
+
+    switch ($data->type) {
+        case 'confirmation':
+            return getenv('VK_CONFIRAMTION_CODE');
+            break;
+
+        case 'message_new':
+            $request_params = [
+                'user_id' => $data->object->userId,
+                'message' => 'тест',
+                'access_token' => getenv('VK_TOKEN'),
+                'v' => '5.68'
+            ];
+
+            file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
+            return 'ok';
+            break;
+    }
+    return "Slomalos'!!!";
 });
 
 $app->run();
